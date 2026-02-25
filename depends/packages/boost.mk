da89328a5cbfcdb334f9d044a4d9d413c3c5ba76
Nathan Slaven <nathan@pacificao.com>
2026-02-25 03:29:51 +0000

    Fix depends config.site PATH and boost layout

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -10,7 +10,7 @@ $(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7c
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=system --user-config=user-config.jam
+$(package)_config_opts=--layout=versioned --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
 $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
 $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
