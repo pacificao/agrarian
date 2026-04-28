44f1fb0435e38764cea2f57abc6823bd703bb4d9
root <root@alltheacronyms.com>
2026-04-28 19:06:11 +0000

    Fix mingw depends host triplets

--- depends/hosts/x86_64-w64-mingw32.mk
+++ depends/hosts/x86_64-w64-mingw32.mk
@@ -3,21 +3,16 @@
 # file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
 host_arch := x86_64
-host_os   := linux
+host_os   := mingw32
 
 host_prefix := $(BASEDIR)/$(HOST)
 build_prefix := $(BASEDIR)/build/$(BUILD)
 
-x86_64_linux_host := $(BUILD)
-x86_64_linux_prefix := $(host_prefix)
-x86_64_linux_id_string := $(HOST)
+x86_64_mingw32_host := $(HOST)
+x86_64_mingw32_prefix := $(host_prefix)
+x86_64_mingw32_id_string := $(HOST)
 
 include hosts/default.mk
-include hosts/linux.mk
+include hosts/mingw32.mk
 
-x86_64_linux_CC := gcc -m64
-x86_64_linux_CXX := g++ -m64
-x86_64_linux_AR := ar
-x86_64_linux_RANLIB := ranlib
-x86_64_linux_NM := nm
-x86_64_linux_STRIP := strip
+x86_64_mingw32_WINDRES := $(HOST)-windres
